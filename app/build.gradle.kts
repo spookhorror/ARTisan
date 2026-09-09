6285e4cd70a65d51a4c52582bbd2e453a7f6b6ce
spookhorror <varunvishwa75@gmail.com>
2026-09-09 09:39:28 +0530

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0-pre-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
